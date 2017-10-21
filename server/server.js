09140393cef34792df5912d8d022757138f87611
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-20 23:16:16 -0400

    Apollo client setup

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://jaypark:jaypark@ds047901.mlab.com:47901/lyricaldb-dev';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
